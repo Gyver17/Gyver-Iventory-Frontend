9acd2a34fbcc0cf8d9dab56d9bcbbbc670da4364
Gyver <joseangel171001@gmail.com>
2022-02-14 19:05:18 -0400

    branch dev

---
+++ src/helpers/requestRejected.jsx
@@ -0,0 +1,14 @@
+import { types } from "../../../../context/authReducer";
+
+const requestRejected = (code, dispatch, toast) => {
+	if (code === "50115" || code === "43292" || code === "43178") {
+		dispatch({
+			type: types.sessionClose,
+		});
+	}
+
+	// This Situation Should Not Happen
+	if (code === "43097") {
+		toast.error("Valores Invalido");
+	}
+};
